c70a2992bc8a0df5a016ee8277d1e3fca0fb1e22
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-20 22:36:23 -0500

    feat(TvModal): add demo components for default, success, error, and info modals

---
+++ src/demo/utils/demos/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="modal.openModal()">Show Error</button>
+
+  <tv-modal
+    :config-modal="modalConfig"
+    @accepted="() => console.log('Accepted')"
+    @canceled="() => console.log('Canceled')"
+    ref="modal"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { TvModal } from "@todovue/tv-modal";
+
+const modal = ref();
+const modalConfig = ref({
+  title: "Error occurred",
+  description: "Something went wrong. Please try again later.",
+  confirmButtonText: "OK",
+  cancelButtonText: "Cancel",
+  icon: "error",
+});
+</script>
---
+++ src/demo/utils/demos/info.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="modal.openModal()">Show Info</button>
+
+  <tv-modal
+    :config-modal="modalConfig"
+    @accepted="() => console.log('Accepted')"
+    ref="modal"
+  />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { TvModal } from "@todovue/tv-modal";
+
+const modal = ref();
+const modalConfig = ref({
+  title: "Information",
+  description: "Here is some important information you should know about.",
+  confirmButtonText: "Got it",
+  icon: "info",
+});
+</script>
