e7c239d4959a0c6d5e96e6de8e1ed60e2a9c596f
Cristhian Daza <cristian.d.b@hotmail.com>
2025-10-20 22:20:08 -0500

    feat(TvModal): enhance modal styling with light and dark mode support

--- src/components/TvModalDemo.vue
+++ src/components/TvModalDemo.vue
@@ -62,11 +62,16 @@ const handleAccept = () => {
 const handleCancel = () => {
   console.log('Modal canceled');
 };
+
+const customStyle = ref({
+  backgroundColor: '#0f2e5b',
+  color: '#fff',
+});
 </script>
 
 <template>
   <div class="tv-modal-demo-wrapper">
-    <tv-button @click="openModal" isSuccess>
+    <tv-button @click="openModal" rounded  :custom-style="customStyle">
       {{ buttonText }}
     </tv-button>
 
@@ -83,5 +88,70 @@ const handleCancel = () => {
 <style scoped>
 .tv-modal-demo-wrapper {
   display: inline-block;
+}
+</style>
+
+<style lang="scss">
+@use "../assets/scss/variables";
+@use "sass:color";
+
+.tv-modal-container.light-mode {
+  background-color: rgba(0, 0, 0, 0.4);
+
+  .tv-modal {
+    background-color: variables.$light-card-bg;
+    box-shadow: 0 20px 60px rgba(0, 0, 0, 0.2),
+    0 0 0 1px rgba(0, 0, 0, 0.08);
+
+    &-title {
+      color: variables.$light-text;
+    }
+
+    &-separator {
+      border-top-color: rgba(variables.$light-text, 0.2);
+    }
+
+    &-description {
+      color: color.adjust(variables.$light-text, $lightness: 15%);
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: rgba(variables.$light-text, 0.3);
+
+      &:hover {
+        background: rgba(variables.$light-text, 0.4);
+      }
+    }
+  }
+}
+
+.tv-modal-container.dark-mode {
+  background-color: rgba(0, 0, 0, 0.7);
+
+  .tv-modal {
+    background-color: variables.$dark-card-bg;
+    box-shadow: 0 20px 60px rgba(0, 0, 0, 0.6),
+    0 0 0 1px rgba(255, 255, 255, 0.05);
+
+    &-title {
+      color: variables.$dark-text;
+    }
+
+    &-separator {
+      border-top-color: rgba(variables.$dark-text, 0.15);
+    }
+
+    &-description {
+      color: color.adjust(variables.$dark-text, $lightness: -20%);
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: rgba(255, 255, 255, 0.2);
+
+      &:hover {
+        background: rgba(255, 255, 255, 0.3);
+      }
+    }
+  }
 }
 </style>
